31feea7f6c8da23a5f7c02e56460ec6259691ffc
Muhammed Mehti GÜLER <mehti.guler@gmail.com>
2024-02-25 22:54:34 +0300

    package update

---
+++ grPcExample/MappingProfile.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using grPcExample.Models;
+using OrderServiceGrpc;
+
+namespace grPcExample
+{
+    public class MappingProfile:Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<CreateOrderRequest, Order>();
+            // Gerekirse ters yönde bir harita da ekleyebilirsiniz:
+            CreateMap<Order, CreateOrderRequest>();
+        }
+    }
+}
--- grPcExample/Models/Order.cs
+++ grPcExample/Models/Order.cs
@@ -3,7 +3,7 @@
     public class Order
     {
         public string Id { get; set; } = Guid.NewGuid().ToString();
-        public string? Name { get; set; }
+        public string Name { get; set; }
         public bool Enable { get; set; } = true;
         public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
     }
